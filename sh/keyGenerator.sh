e2d34ee2de1e88c042538ba3d1a9b6573fb1e7e7
tanbbang01@gmail.com <tanbbang01@gmail.com>
2021-03-31 22:52:39 +0900

    Add shell KeyGenerator

---
+++ sh/keyGenerator.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_80.jdk/Contents/Home
+ENCRYPTION_HOME=/Users/inseong/Develop/test
+
+java -classpath /Users/inseong/Develop/test/encryption-deploy.jar kr.inek.encrypt.generator.KeyGenerator
